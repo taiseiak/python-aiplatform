5cc683e3319dbbf4a4eac14793e1a882af007983
taiseiak <taiseiklasen@gmail.com>
2021-12-12 20:54:29 -0800

    feat: Add XAI SDK integration to TensorFlow models with LIT integration
    Add automatic addition of feature attribution for TensorFlow 2 models in the LIT integration on Vertex Notebooks. Detects for Vertex Notebooks by looking for the same enviornment variable to check for Vertex Notebooks as the LIT library does.

--- google/cloud/aiplatform/explain/lit.py
+++ google/cloud/aiplatform/explain/lit.py
@@ -15,11 +15,13 @@
 # limitations under the License.
 
 from typing import Dict, List, Tuple, Union
+import os
 
 try:
     from lit_nlp.api import dataset as lit_dataset
     from lit_nlp.api import model as lit_model
     from lit_nlp.api import types as lit_types
+    from lit_nlp.api import dtypes as lit_dtypes
     from lit_nlp import notebook
 except ImportError:
     raise ImportError(
@@ -81,6 +83,7 @@ class _VertexLitModel(lit_model.Model):
         model: str,
         input_types: "OrderedDict[str, lit_types.LitType]",  # noqa: F821
         output_types: "OrderedDict[str, lit_types.LitType]",  # noqa: F821
+        attribution_method: str = "sampled_shapley",
     ):
         """Construct a VertexLitModel.
             Args:
@@ -93,22 +96,17 @@ class _VertexLitModel(lit_model.Model):
               output_types:
                 Required. An OrderedDict of string names matching the labels of the model
                 as the key, and the associated LitType of the label.
+              attribution_method:
+                Optional. A string to choose what attribution configuration to
+                set up the explainer with. Valid options are 'sampled_shapley'
+                or 'integrated_gradients'.
         """
-        self._loaded_model = tf.saved_model.load(model)
-        serving_default = self._loaded_model.signatures[
-            tf.saved_model.DEFAULT_SERVING_SIGNATURE_DEF_KEY
-        ]
-        _, self._kwargs_signature = serving_default.structured_input_signature
-        self._output_signature = serving_default.structured_outputs
-
-        if len(self._kwargs_signature) != 1:
-            raise ValueError("Please use a model with only one input tensor.")
-
-        if len(self._output_signature) != 1:
-            raise ValueError("Please use a model with only one output tensor.")
-
+        self._load_model(model)
         self._input_types = input_types
         self._output_types = output_types
+        self._input_tensor_name = next(iter(self._kwargs_signature))
+        if os.environ.get("LIT_PROXY_URL"):
+            self._set_up_attribution_explainer(model, attribution_method)
 
     def predict_minibatch(
         self, inputs: List[lit_types.JsonDict]
@@ -118,7 +116,7 @@ class _VertexLitModel(lit_model.Model):
             instance = [input[feature] for feature in self._input_types]
             instances.append(instance)
         prediction_input_dict = {
-            next(iter(self._kwargs_signature)): tf.convert_to_tensor(instances)
+            self._input_tensor_name: tf.convert_to_tensor(instances)
         }
         prediction_dict = self._loaded_model.signatures[
             tf.saved_model.DEFAULT_SERVING_SIGNATURE_DEF_KEY
@@ -132,13 +130,67 @@ class _VertexLitModel(lit_model.Model):
                     for label, value in zip(self._output_types.keys(), prediction)
                 }
             )
+        # Get feature attributions
+        if hasattr(self, "_attribution_explainer"):
+            attributions = self._attribution_explainer.explain(
+                [{self._input_tensor_name: i} for i in instances]
+            )
+            for i, attribution in enumerate(attributions):
+                outputs[i]["feature_attribution"] = lit_dtypes.FeatureSalience(
+                    attribution.feature_importance()
+                )
         return outputs
 
     def input_spec(self) -> lit_types.Spec:
         return dict(self._input_types)
 
     def output_spec(self) -> lit_types.Spec:
-        return self._output_types
+        output_spec_dict = dict(self._output_types)
+        if hasattr(self, "_attribution_explainer"):
+            output_spec_dict["feature_attribution"] = lit_types.FeatureSalience(
+                signed=True
+            )
+        return output_spec_dict
+
+    def _load_model(self, model: str):
+        self._loaded_model = tf.saved_model.load(model)
+        serving_default = self._loaded_model.signatures[
+            tf.saved_model.DEFAULT_SERVING_SIGNATURE_DEF_KEY
+        ]
+        _, self._kwargs_signature = serving_default.structured_input_signature
+        self._output_signature = serving_default.structured_outputs
+
+        if len(self._kwargs_signature) != 1:
+            raise ValueError("Please use a model with only one input tensor.")
+
+        if len(self._output_signature) != 1:
+            raise ValueError("Please use a model with only one output tensor.")
+
+    def _set_up_attribution_explainer(
+        self, model: str, attribution_method: str = "integrated_gradients"
+    ):
+        try:
+            import explainable_ai_sdk
+            from explainable_ai_sdk.metadata.tf.v2 import SavedModelMetadataBuilder
+        except ImportError:
+            return
+
+        builder = SavedModelMetadataBuilder(model)
+        builder.get_metadata()
+        builder.set_numeric_metadata(
+            self._input_tensor_name,
+            index_feature_mapping=list(self._input_types.keys()),
+        )
+        builder.save_metadata(model)
+        if attribution_method == "integrated_gradients":
+            explainer_config = explainable_ai_sdk.IntegratedGradientsConfig()
+        else:
+            explainer_config = explainable_ai_sdk.SampledShapleyConfig()
+
+        self._attribution_explainer = explainable_ai_sdk.load_model_from_local_path(
+            model, explainer_config
+        )
+        self._load_model(model)
 
 
 def create_lit_dataset(
@@ -162,22 +214,27 @@ def create_lit_model(
     model: str,
     input_types: "OrderedDict[str, lit_types.LitType]",  # noqa: F821
     output_types: "OrderedDict[str, lit_types.LitType]",  # noqa: F821
+    attribution_method: str = "sampled_shapley",
 ) -> lit_model.Model:
     """Creates a LIT Model object.
         Args:
           model:
-              Required. A string reference to a local TensorFlow saved model directory.
-              The model must have at most one input and one output tensor.
+            Required. A string reference to a local TensorFlow saved model directory.
+            The model must have at most one input and one output tensor.
           input_types:
-              Required. An OrderedDict of string names matching the features of the model
-              as the key, and the associated LitType of the feature.
+            Required. An OrderedDict of string names matching the features of the model
+            as the key, and the associated LitType of the feature.
           output_types:
-              Required. An OrderedDict of string names matching the labels of the model
-              as the key, and the associated LitType of the label.
+            Required. An OrderedDict of string names matching the labels of the model
+            as the key, and the associated LitType of the label.
+          attribution_method:
+            Optional. A string to choose what attribution configuration to
+            set up the explainer with. Valid options are 'sampled_shapley'
+            or 'integrated_gradients'.
         Returns:
             A LIT Model object that has the same functionality as the model provided.
     """
-    return _VertexLitModel(model, input_types, output_types)
+    return _VertexLitModel(model, input_types, output_types, attribution_method)
 
 
 def open_lit(
@@ -188,11 +245,11 @@ def open_lit(
     """Open LIT from the provided models and datasets.
         Args:
           models:
-              Required. A list of LIT models to open LIT with.
+            Required. A list of LIT models to open LIT with.
           input_types:
-              Required. A lit of LIT datasets to open LIT with.
+            Required. A lit of LIT datasets to open LIT with.
           open_in_new_tab:
-              Optional. A boolean to choose if LIT open in a new tab or not.
+            Optional. A boolean to choose if LIT open in a new tab or not.
         Raises:
             ImportError if LIT is not installed.
     """
@@ -206,24 +263,31 @@ def set_up_and_open_lit(
     model: Union[str, lit_model.Model],
     input_types: Union[List[str], Dict[str, lit_types.LitType]],
     output_types: Union[str, List[str], Dict[str, lit_types.LitType]],
+    attribution_method: str = "sampled_shapley",
     open_in_new_tab: bool = True,
 ) -> Tuple[lit_dataset.Dataset, lit_model.Model]:
     """Creates a LIT dataset and model and opens LIT.
         Args:
-        dataset:
+          dataset:
             Required. A Pandas DataFrame that includes feature column names and data.
-        column_types:
+          column_types:
             Required. An OrderedDict of string names matching the columns of the dataset
             as the key, and the associated LitType of the column.
-        model:
+          model:
             Required. A string reference to a TensorFlow saved model directory.
             The model must have at most one input and one output tensor.
-        input_types:
+          input_types:
             Required. An OrderedDict of string names matching the features of the model
             as the key, and the associated LitType of the feature.
-        output_types:
+          output_types:
             Required. An OrderedDict of string names matching the labels of the model
             as the key, and the associated LitType of the label.
+          attribution_method:
+            Optional. A string to choose what attribution configuration to
+            set up the explainer with. Valid options are 'sampled_shapley'
+            or 'integrated_gradients'.
+          open_in_new_tab:
+            Optional. A boolean to choose if LIT open in a new tab or not.
         Returns:
             A Tuple of the LIT dataset and model created.
         Raises:
@@ -234,8 +298,12 @@ def set_up_and_open_lit(
         dataset = create_lit_dataset(dataset, column_types)
 
     if not isinstance(model, lit_model.Model):
-        model = create_lit_model(model, input_types, output_types)
+        model = create_lit_model(
+            model, input_types, output_types, attribution_method=attribution_method
+        )
 
-    open_lit({"model": model}, {"dataset": dataset}, open_in_new_tab=open_in_new_tab)
+    open_lit(
+        {"model": model}, {"dataset": dataset}, open_in_new_tab=open_in_new_tab,
+    )
 
     return dataset, model
